6bb8a19a0ca33dcb618bcabe83d56418659ec9d8
ram patel <ramvijaypatel96@gmail.com>
2025-03-05 15:08:12 +0530

    new order place

---
+++ admin/new_order.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<!--
+Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 3.3.7
+Version: 4.7.1
+Author: KeenThemes
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+
+<head>
+    <meta charset="utf-8" />
+    <title>Metronic Admin Theme #4 | Bootstrap Form Controls</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="Preview page of Metronic Admin Theme #4 for bootstrap inputs, input groups, custom checkboxes and radio controls and more" name="description" />
+    <meta content="" name="author" />
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+    <link href="../assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <!-- BEGIN THEME GLOBAL STYLES -->
+    <link href="../assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
+    <link href="../assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
+    <!-- END THEME GLOBAL STYLES -->
+    <!-- BEGIN THEME LAYOUT STYLES -->
+    <link href="../assets/layouts/layout4/css/layout.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/layouts/layout4/css/themes/default.min.css" rel="stylesheet" type="text/css" id="style_color" />
+    <link href="../assets/layouts/layout4/css/custom.min.css" rel="stylesheet" type="text/css" />
+    <!-- END THEME LAYOUT STYLES -->
+    <link rel="shortcut icon" href="favicon.ico" />
+</head>
+<!-- END HEAD -->
+
+<body class="page-container-bg-solid page-header-fixed page-sidebar-closed-hide-logo">
+    <!-- BEGIN HEADER -->
+    <?php
+    include "./header.php";
+    ?>
+    <!-- END HEADER -->
+    <!-- BEGIN HEADER & CONTENT DIVIDER -->
+    <div class="clearfix"> </div>
+    <!-- END HEADER & CONTENT DIVIDER -->
+    <!-- BEGIN CONTAINER -->
+    <div class="page-container">
+        <!-- BEGIN SIDEBAR -->
+        <?php
+        include "./sidebar.php";
+        ?>
+        <!-- END SIDEBAR -->
+        <!-- BEGIN CONTENT -->
+        <div class="page-content-wrapper">
+            <!-- BEGIN CONTENT BODY -->
+            <div class="page-content">
+                <!-- BEGIN PAGE HEAD-->
+                <div class="page-head">
+                    <!-- BEGIN PAGE TITLE -->
+                    <div class="page-title">
+                        <h1>Add New Order
+                        </h1>
+                    </div>
+                    <!-- END PAGE TITLE -->
+                </div>
+                <!-- END PAGE HEAD-->
+                <!-- BEGIN PAGE BASE CONTENT -->
+                <div class="row">
+                    <div class="col-md-12 ">
+                        <!-- BEGIN SAMPLE FORM PORTLET-->
+                        <div class="portlet light bordered">
+                            <div class="portlet-body form">
+                                <form action="" method="post" enctype="multipart/form-data" role="form">
+                                    <div class="form-body">
+                                        <div class="col-md-6">
+                                            <h3 style="padding:5px 0px;font-weight: 500;">New Order</h3>
+                                            <div class="form-group">
+                                                <label for="Customer">Customer<span class="required">*</span></label>
+                                                <select class="form-control" name="customer_id" required>
+                                                    <option>Select Customer</option>
+                                                    <?php
+                                                    include "../connection.php";
+                                                    $query = $conn->prepare("select * from customers");
+                                                    $query->execute();
+                                                    // Fetch all records
+                                                    $customers = $query->fetchAll();
+                                                    foreach ($customers as $customer) {
+                                                        echo '<option value=" ' . $customer["id"] . '" data-mobile_number="' . $customer["mobile_number"] . '" data-email="' . $customer["email"] . '" data-address="' . $customer["address"] . '" data-city="' . $customer["city"] . '" > ' . $customer["name"] . '-' . $customer["mobile_number"] . '</option>';
+                                                    }
+                                                    ?>
+                                                </select>
+
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="mobile_no">Mobile No.<span class="required">*</span></label>
+                                                <input type="text" name="customer_mno" id="customer_mno" class="form-control" value="" placeholder="Customer Mobile no" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="email">Email ID.</label>
+                                                <input type="text" name="customer_email" id="customer_email" class="form-control" value="" placeholder="Customer Email" requried>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="city">City</label>
+                                                <input type="text" name="customer_city" id="customer_city" class="form-control" value="" placeholder="Customer city" requried>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="address">Address<span class="required">*</span></label>
+                                                <textarea class="form-control" placeholder="Customer Addres" id="customer_address" name="customer_address" required></textarea>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="pincode">Pincode<span class="required">*</span></label>
+                                                <input type="text" name="customer_pincode" id="customer_pincode" class="form-control" value="" placeholder="Customer Pincode" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="delivery_time">Delivery Time<span class="required">*</span></label>
+                                                <select class="select2me form-control" name="delivery_time_slot" required>
+                                                    <option value="">Select Delivery Time Slot</option>
+                                                    <option value="2"> 03:00 PM To 05:00 PM</option>
+                                                    <option value="3"> 10:00 AM To 01:00 PM</option>
+                                                    <option value="1"> 10:00 AM To 09:00 PM</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="discount">Discount<span class="required">*</span></label>
+                                                <input type="text" name="invoice_discount" class="form-control" value="0" placeholder="Discount">
+                                                <span class="help-block" id="point_message"></span>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="delivery_charge">Delivery Charge<span class="required">*</span></label>
+                                                <input type="text" name="invoice_delivery_charges" class="form-control" value="0" placeholder="Delivery Charge">
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="special_instruction">Special Instruction</label>
+                                                <textarea class="form-control" placeholder="Special Instruction" id="invoice_special_instruction" name="invoice_special_instruction" requried></textarea>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="sms">Send sms </label>
+                                                <div class="mt-checkbox-inline">
+                                                    <label class="mt-checkbox">
+                                                        <input type="checkbox" id="send_sms" name="send_sms" value="1"> YES
+                                                        <span></span>
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-actions">
+                                            <button type="submit" name="submit" class="btn blue">Add Brand</button>
+                                            <button class="btn btn-success"><a href="brandlist.php" style="text-decoration: none;color:white">Cancel</a></button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <!-- END SAMPLE FORM PORTLET-->
+                    </div>
+
+                </div>
+
+                <!-- END PAGE BASE CONTENT -->
+            </div>
+            <!-- END CONTENT BODY -->
+        </div>
+        <!-- END CONTENT -->
+    </div>
+    <!-- END CONTAINER -->
+    <!-- BEGIN FOOTER -->
+    <?php
+    include "./footer.php";
+    ?>
+    <!-- END FOOTER -->
+    <script src="../assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="../assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
+    <script src="../assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+    <script src="../assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+    <script src="../assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+    <!-- END CORE PLUGINS -->
+    <!-- BEGIN PAGE LEVEL PLUGINS -->
+    <script src="../assets/global/plugins/bootstrap-select/js/bootstrap-select.min.js" type="text/javascript"></script>
+    <!-- END PAGE LEVEL PLUGINS -->
+    <!-- BEGIN THEME GLOBAL SCRIPTS -->
+    <script src="../assets/global/scripts/app.min.js" type="text/javascript"></script>
+    <!-- END THEME GLOBAL SCRIPTS -->
+    <!-- BEGIN PAGE LEVEL SCRIPTS -->
+    <script src="../assets/pages/scripts/components-bootstrap-select.min.js" type="text/javascript"></script>
+    <!-- END PAGE LEVEL SCRIPTS -->
+    <!-- BEGIN THEME LAYOUT SCRIPTS -->
+    <script src="../assets/layouts/layout4/scripts/layout.min.js" type="text/javascript"></script>
+    <script src="../assets/layouts/layout4/scripts/demo.min.js" type="text/javascript"></script>
+    <script src="../assets/layouts/global/scripts/quick-sidebar.min.js" type="text/javascript"></script>
+    <script src="../assets/layouts/global/scripts/quick-nav.min.js" type="text/javascript"></script>
+    <!-- BEGIN CORE PLUGINS -->
+    <script src="../assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+    <script src="../assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="../assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
+    <script src="../assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+    <script src="../assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+    <script src="../assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+    <!-- END CORE PLUGINS -->
+    <!-- BEGIN THEME GLOBAL SCRIPTS -->
+    <script src="../assets/global/scripts/app.min.js" type="text/javascript"></script>
+    <!-- END THEME GLOBAL SCRIPTS -->
+    <!-- BEGIN THEME LAYOUT SCRIPTS -->
+    <script src="../assets/layouts/layout4/scripts/layout.min.js" type="text/javascript"></script>
+    <script src="../assets/layouts/layout4/scripts/demo.min.js" type="text/javascript"></script>
+    <script src="../assets/layouts/global/scripts/quick-sidebar.min.js" type="text/javascript"></script>
+    <script src="../assets/layouts/global/scripts/quick-nav.min.js" type="text/javascript"></script>
+
+    <script>
+        $(document).ready(function() {
+            $(document).on("change", "select[name='customer_id']", function() {
+                var selected = $(this).find(":selected"); // Get selected option
+
+                // Fetch customer details from data attributes
+                var mobile_number = selected.data("mobile_number");
+                var email = selected.data("email");
+                var city = selected.data("city");
+                var address = selected.data("address");
+
+                // Update input fields
+                $("#customer_mno").val(mobile_number);
+                $("#customer_email").val(email);
+                $("#customer_city").val(city);
+                $("#customer_address").val(address);
+            });
+        });
+    </script>
+
+    <!-- END THEME LAYOUT SCRIPTS -->
+
+</body>
+
+</html>
